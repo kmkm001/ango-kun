7871b26b198397c5aafcc959ae85b1747359c499
kmkm001 <kmkmdev@gmail.com>
2015-08-24 16:54:56 +0900

    first commit

---
+++ ango.js
@@ -0,0 +1,34 @@
+﻿/* ango.js */
+
+var crypto = require('crypto');
+
+/**
+* 定数
+*/
+const CRYPTO_ALGORITHM = 'aes128';//暗号化アルゴリズム
+const STRING_ENCODE = 'utf8'; //BASE64encode
+const CRYPTO_ENCODE = 'hex';  //暗号化encode
+
+
+/**
+* CRYPTO_ALGOTITHMアルゴリズムを用いて
+* CRYPTO_ENCODEを文字列に復号化する
+*/
+exports.DecipherString = function(string, passwd) {
+  var decipher = crypto.createDecipher(CRYPTO_ALGORITHM, passwd);
+  var dec = decipher.update(string, CRYPTO_ENCODE, STRING_ENCODE);
+  dec += decipher.final(STRING_ENCODE);
+  return dec;
+}
+
+
+/**
+* CRYPTO_ALGOTITHMアルゴリズムを用いて
+* 文字列をCRYPTO_ENCODEに暗号化する
+*/
+exports.CipherString = function(string, passwd) {
+  var cipher = crypto.createCipher(CRYPTO_ALGORITHM, passwd);
+  var cip = cipher.update(string, STRING_ENCODE, CRYPTO_ENCODE);
+  cip += cipher.final(CRYPTO_ENCODE);
+  return cip;
+}
---
+++ app.js
@@ -0,0 +1,69 @@
+﻿/* app.js */
+
+/**
+* 追加モジュール
+*/
+var fs = require('fs');
+var ango = require('./ango');
+
+/**
+* 定数
+*/
+// モードの種類
+const MODE_NONE = 0
+const MODE_ENCODE = 1;
+const MODE_DECODE = 2;
+
+/**
+* 処理
+*/
+// 引数チェック
+if (process.argv.length < 6) {
+  console.log('missing argument.');
+  return;
+}
+
+// 引数の内容取得
+// 実行モード
+var mode;
+if(process.argv[2] == '/e'){
+  mode = MODE_ENCODE;
+}else if(process.argv[2] == '/d'){
+  mode = MODE_DECODE;
+}else{
+  mode = MODE_NONE;
+}
+// 入力ファイル
+var import_file = process.argv[3];
+// 出力ファイル
+var export_file = process.argv[4];
+// パスワード
+var password = process.argv[5];
+
+// 実処理部
+// 指定のモードなし
+if(mode == MODE_NONE){
+  console.log('モード指定がありません');
+}
+// エンコード
+else if(mode == MODE_ENCODE){
+  // read import file
+  var import_data = fs.readFileSync(import_file);
+  // data encode
+  var tmp_data = new Buffer(import_data).toString('base64');
+  var export_data = ango.CipherString(tmp_data, password);
+  // write export file
+  fs.writeFile(export_file, export_data);
+}
+// デコード
+else if(mode == MODE_DECODE){
+  // read import file
+  var import_data = fs.readFileSync(import_file, 'utf-8');
+  // console.log(import_data);
+  // data decode 
+  var tmp_data = ango.DecipherString(import_data, password);
+  var export_data = new Buffer(tmp_data, 'base64');
+  // write export file
+  fs.writeFile(export_file, export_data);
+}
+
